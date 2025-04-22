ad6aebf728869bc6b943380bff73d270bfc25e51
mmal3k <mmalekrais@gmail.com>
2025-04-22 17:07:17 +0200

    add cors origin env variable

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
